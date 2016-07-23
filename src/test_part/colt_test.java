b21f45cfe89c1efd420053304babb023692c4962
multiangle <461445957@qq.com>
2016-07-23 20:55:59 +0800

    Initial commit

---
+++ src/test_part/colt_test.java
@@ -0,0 +1,15 @@
+package test_part;
+
+import cern.colt.matrix.DoubleMatrix2D ;
+import cern.colt.matrix.impl.SparseDoubleMatrix2D ;
+import cern.colt.matrix.ObjectMatrix2D ;
+
+/**
+ * Created by multiangle on 2016/7/23.
+ */
+public class colt_test {
+    public static void main(String[] args){
+        DoubleMatrix2D matrix ;
+        matrix = new SparseDoubleMatrix2D(5,6);
+}
+}
---
+++ src/test_part/time_test.java
@@ -0,0 +1,7 @@
+package test_part;
+
+/**
+ * Created by multiangle on 2016/7/23.
+ */
+public class time_test {
+}
