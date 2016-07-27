e380630efe004950cb6748cd8d042b2bef3bef68
multiangle <461445957@qq.com>
2016-07-27 16:48:30 +0800

    完成统计词频部分，不过未经测试

--- src/ItemFreqCount/SingleWordFreq.java
+++ src/ItemFreqCount/SingleWordFreq.java
@@ -20,13 +20,16 @@ public class SingleWordFreq {
     private ArrayList<WordCountElement> freq_month ;
     private ArrayList<WordCountElement> freq_year ;
 
-    SingleWordFreq(String word, int dict_id){
+    private TopWordCount top_word_counter ; // 统计TOPN
+
+    SingleWordFreq(String word, int dict_id, TopWordCount top_word_counter){
         this.word = word ;
         this.dict_id = dict_id ;
         this.total_freq = 0 ;
         this.freq_day = new ArrayList<WordCountElement>() ;
         this.freq_month = new ArrayList<WordCountElement>() ;
         this.freq_year = new ArrayList<WordCountElement>() ;
+        this.top_word_counter = top_word_counter ;
     }
 
     // 这一部分实现的是在array中查找到规定的时间戳对象，并加相应值
@@ -36,22 +39,23 @@ public class SingleWordFreq {
     private void _findAndAdd(ArrayList<WordCountElement> list, long timestamp, int N){
         int index = BinaryFindAsDate.findAsTimestamp(list,timestamp) ;
         if (index == list.size()){ // 如果查找到的结果超出范围，则新增一个节点
-            WordCountElement one = new WordCountElement(this.dict_id,timestamp) ;
-            list.add(one);
-        } else if (list.get(index).getTimestamp()==timestamp){
+            WordCountElement wce = new WordCountElement(this.dict_id,timestamp) ;
+            list.add(wce);
+            this.top_word_counter.addSingleWord(wce);
+        } else if (list.get(index).getTimestamp()==timestamp){ // 如果节点已经存在，则自加N
             list.get(index).addFreq(N);
         } else{
-            list.add(index,new WordCountElement(this.dict_id,timestamp));
+            WordCountElement wce = new WordCountElement(this.dict_id,timestamp) ;
+            list.add(index,wce);
+            this.top_word_counter.addSingleWord(wce);
         }
     }
 
     // 当输入相应的时间戳，则对对象中各个值做相应处理。包括total_freq,freq_day/mon/year 等
-    public void addByPyTimestamp_sec(int timestamp){
+    public void addByTimestamp_sec(long timestamp){
         addByTimestamp_sec(timestamp, 1);
     }
-    public void addByTimestamp_sec(int timestamp, int N){
-        // 这里的timestamp是python格式的,即以秒为单位，这里需要改成java格式的，即以毫秒为单位
-        timestamp *= 1000 ;
+    public void addByTimestamp_sec(long timestamp, int N){
         Date[] certain_dates = WordCountElement.getCertainDates(timestamp) ;
         Date day = certain_dates[0] ;
         Date mon = certain_dates[1] ;
@@ -60,7 +64,6 @@ public class SingleWordFreq {
         _findAndAdd(this.freq_day, day, N);
         _findAndAdd(this.freq_month, mon, N);
         _findAndAdd(this.freq_year, year, N);
-
     }
 
 }
--- src/ItemFreqCount/WordCount.java
+++ src/ItemFreqCount/WordCount.java
@@ -1,8 +1,35 @@
 package ItemFreqCount;
 
+import java.util.HashMap;
+
 /**
  * Created by multiangle on 2016/7/24.
  */
 public class WordCount {
+    private TopWordCount top_word_counter ;
+    private HashMap<String, Integer> word_dict ;
+    private HashMap<String, SingleWordFreq> word_freq ;
 
+    WordCount(){
+        top_word_counter = new TopWordCount() ; // 每日流行词统计
+        word_dict = new HashMap<String, Integer>() ; // 单词-id映射对
+        word_freq = new HashMap<String, SingleWordFreq>() ; // 存放单个词语的各天趋势变化
+    }
+
+    public void addWordWithTimestamp(String word, long timestamp){ //这里假设 timestamp已经是Java形式
+        Integer id = word_dict.get(word) ;
+        int dict_id ;
+        SingleWordFreq swf ;
+        if (id!=null) {
+            dict_id = id ;
+            swf = word_freq.get(word) ;
+        }
+        else{
+            dict_id = word_dict.size() ;
+            word_dict.put(word,dict_id) ;
+            swf = new SingleWordFreq(word,dict_id,this.top_word_counter) ;
+            word_freq.put(word,swf) ;
+        }
+        swf.addByTimestamp_sec(timestamp); // 向single word freq 加入该时间戳
+    }
 }
